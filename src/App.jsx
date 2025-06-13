9fc47d0e43749ee85e1c29afd8a614f617169194
Josue Villalona <josuevillalona@gmail.com>
2025-06-13 02:10:09 -0400

    Fix duplicate imports in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-import { useState } from 'react';
-import Map from './Map';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import React, { useState } from 'react';
 import './App.css';
 import Map from './Map';
